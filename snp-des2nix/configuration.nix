1f2ac80f110322e22dc55749c708f1c45a7b4956
snuppy <snuppy.code@pm.me>
2026-01-25 03:29:56 +0100

    nextcloud https & rebase

--- snp-des2nix/configuration.nix
+++ snp-des2nix/configuration.nix
@@ -7,7 +7,6 @@
 {
 	imports = [
 	];
-	
 	sops.secrets.mend-password.neededForUsers = true;
 	users.users.mend = {
 		isNormalUser = true;
@@ -15,60 +14,73 @@
 		hashedPasswordFile = config.sops.secrets.mend-password.path;
 	};
 
-	environment.etc."nextcloud-admin-pass".text = "changeme";
-	services.nextcloud = {
-			enable = true;
-			package = pkgs.nextcloud31;
-			hostName = "snp-des2nix.tailf46592.ts.net";
-			database.createLocally = true;
-			config.adminpassFile = "/etc/nextcloud-admin-pass";
-			config.dbtype = "pgsql";
-	};
+        environment.etc."nextcloud-admin-pass".text = "changeme";
+        services.nextcloud = {
+                enable = true;
+                package = pkgs.nextcloud31;
+                hostName = "snp-des2nix.tailf46592.ts.net";
+                https = true;
+                database.createLocally = true;
+                config.adminpassFile = "/etc/nextcloud-admin-pass";
+                config.dbtype = "pgsql";
+        };
 
-	stylix.enable = true;
-	stylix.autoEnable = false;
-	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-	stylix.image = ./../wallpapers/highres/wp3.png;
-	# The generated color scheme can be viewed at /etc/stylix/palette.html on NixOS, or at ~/.config/stylix/palette.html on Home Manager.
-	#stylix.polarity = "dark";
-	stylix.fonts = {
-			#serif = {
-			#  package = pkgs.dejavu_fonts;
-			#  name = "DejaVu Serif";
-			#};
-			#sansSerif = {
-			#  package = pkgs.dejavu_fonts;
-			#  name = "DejaVu Sans";
-			#};
-			monospace = {
-					package = pkgs.nerd-fonts._0xproto;
-					name = "0xProto Nerd Font";
-			};
-			#emoji = {
-			#  package = pkgs.noto-fonts-color-emoji;
-			#  name = "Noto Color Emoji";
-			#};
-	};
-	fonts.packages = with pkgs; [
-			nerd-fonts._0xproto
-					nerd-fonts.jetbrains-mono
-					nerd-fonts.adwaita-mono
-					nerd-fonts.agave
-					nerd-fonts.arimo
-					nerd-fonts.aurulent-sans-mono
-					nerd-fonts.bigblue-terminal
-					nerd-fonts.caskaydia-mono
-					nerd-fonts.commit-mono
-					nerd-fonts.departure-mono
-					nerd-fonts.dejavu-sans-mono
-					nerd-fonts.go-mono
-					nerd-fonts.inconsolata
-					nerd-fonts.iosevka-term
-					nerd-fonts.iosevka-term-slab
-					nerd-fonts.overpass
-					nerd-fonts.sauce-code-pro
-					nerd-fonts.tinos
-	];
+        #services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+        #        forceSSL = true;
+        #        
+        #        sslCertificate = "/var/lib/nginx/certs/nextcloud.crt";#"/var/lib/tailscale/certs/snp-des2nix.tailf46592.ts.net.crt";
+        #        sslCertificateKey = "/var/lib/nginx/certs/nextcloud.key";#"/var/lib/tailscale/certs/snp-des2nix.tailf46592.ts.net.key";
+        #        
+        #        # your cat said oh no no no girl don't even think about it
+        #        enableACME = false;
+        #};
+
+        networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+        stylix.enable = true;
+        stylix.autoEnable = false;
+        stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+        stylix.image = ./../wallpapers/highres/wp3.png;
+        # The generated color scheme can be viewed at /etc/stylix/palette.html on NixOS, or at ~/.config/stylix/palette.html on Home Manager.
+        #stylix.polarity = "dark";
+        stylix.fonts = {
+                #serif = {
+                #  package = pkgs.dejavu_fonts;
+                #  name = "DejaVu Serif";
+                #};
+                #sansSerif = {
+                #  package = pkgs.dejavu_fonts;
+                #  name = "DejaVu Sans";
+                #};
+                monospace = {
+                        package = pkgs.nerd-fonts._0xproto;
+                        name = "0xProto Nerd Font";
+                };
+                #emoji = {
+                #  package = pkgs.noto-fonts-color-emoji;
+                #  name = "Noto Color Emoji";
+                #};
+        };
+        fonts.packages = with pkgs; [
+                nerd-fonts._0xproto
+                        nerd-fonts.jetbrains-mono
+                        nerd-fonts.adwaita-mono
+                        nerd-fonts.agave
+                        nerd-fonts.arimo
+                        nerd-fonts.aurulent-sans-mono
+                        nerd-fonts.bigblue-terminal
+                        nerd-fonts.caskaydia-mono
+                        nerd-fonts.commit-mono
+                        nerd-fonts.departure-mono
+                        nerd-fonts.dejavu-sans-mono
+                        nerd-fonts.go-mono
+                        nerd-fonts.inconsolata
+                        nerd-fonts.iosevka-term
+                        nerd-fonts.iosevka-term-slab
+                        nerd-fonts.overpass
+                        nerd-fonts.sauce-code-pro
+                        nerd-fonts.tinos
+        ];
 
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
