9c32f5412a33325fb8e37e7d643ed7b66f19c785
snuppy <snuppy.code@pm.me>
2026-04-26 16:12:15 +0200

    nextcloud minor improvements

--- snp-des2nix/configuration.nix
+++ snp-des2nix/configuration.nix
@@ -1,181 +1,187 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-	imports = [
-	];
-	sops.secrets.mend-password.neededForUsers = true;
-	users.users.mend = {
-		isNormalUser = true;
-		extraGroups = [ "networkmanager" "wheel" ];
-		hashedPasswordFile = config.sops.secrets.mend-password.path;
-	};
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+  ];
+  sops.secrets.mend-password.neededForUsers = true;
+  users.users.mend = {
+    isNormalUser = true;
+    extraGroups = ["networkmanager" "wheel"];
+    hashedPasswordFile = config.sops.secrets.mend-password.path;
+  };
 
-        sops.secrets.snp-des2nix-key.owner = "nginx";
-        sops.secrets.snp-des2nix-crt.owner = "nginx";
+  sops.secrets.snp-des2nix-key.owner = "nginx";
+  sops.secrets.snp-des2nix-crt.owner = "nginx";
 
-        environment.etc."nextcloud-admin-pass".text = "changeme";
-        services.nextcloud = {
-                enable = true;
-                package = pkgs.nextcloud32;
-                hostName = "snp-des2nix.tailf46592.ts.net";
-                https = true;
-                database.createLocally = true;
-                config.adminpassFile = "/etc/nextcloud-admin-pass";
-                config.dbtype = "pgsql";
-        };
-        services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
-                forceSSL = true;
-                
-                sslCertificate = config.sops.secrets.snp-des2nix-crt.path;
-                sslCertificateKey = config.sops.secrets.snp-des2nix-key.path;
-                
-                # your cat said oh no no no girl don't even think about it
-                enableACME = false;
-        };
+  environment.etc."nextcloud-admin-pass".text = "changeme";
+  services.nextcloud = {
+    enable = true;
+    package = pkgs.nextcloud32;
+    hostName = "snp-des2nix.tailf46592.ts.net";
+    https = true;
+    database.createLocally = true;
+    config.adminpassFile = "/etc/nextcloud-admin-pass";
+    config.dbtype = "pgsql";
+    settings = {
+      maintenance_window_start = 5;
+    };
+    phpOptions = {
+      "opcache.interned_strings_buffer" = "16"; # make a warning go away
+    };
+  };
+  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+    forceSSL = true;
 
-        networking.firewall.allowedTCPPorts = [ 80 443 ];
+    sslCertificate = config.sops.secrets.snp-des2nix-crt.path;
+    sslCertificateKey = config.sops.secrets.snp-des2nix-key.path;
 
-        stylix.enable = true;
-        stylix.autoEnable = false;
-        stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
-        stylix.image = ./../wallpapers/highres/wp3.png;
-        # The generated color scheme can be viewed at /etc/stylix/palette.html on NixOS, or at ~/.config/stylix/palette.html on Home Manager.
-        #stylix.polarity = "dark";
-        stylix.fonts = {
-                #serif = {
-                #  package = pkgs.dejavu_fonts;
-                #  name = "DejaVu Serif";
-                #};
-                #sansSerif = {
-                #  package = pkgs.dejavu_fonts;
-                #  name = "DejaVu Sans";
-                #};
-                monospace = {
-                        package = pkgs.nerd-fonts._0xproto;
-                        name = "0xProto Nerd Font";
-                };
-                #emoji = {
-                #  package = pkgs.noto-fonts-color-emoji;
-                #  name = "Noto Color Emoji";
-                #};
-        };
-        fonts.packages = with pkgs; [
-                nerd-fonts._0xproto
-                        nerd-fonts.jetbrains-mono
-                        nerd-fonts.adwaita-mono
-                        nerd-fonts.agave
-                        nerd-fonts.arimo
-                        nerd-fonts.aurulent-sans-mono
-                        nerd-fonts.bigblue-terminal
-                        nerd-fonts.caskaydia-mono
-                        nerd-fonts.commit-mono
-                        nerd-fonts.departure-mono
-                        nerd-fonts.dejavu-sans-mono
-                        nerd-fonts.go-mono
-                        nerd-fonts.inconsolata
-                        nerd-fonts.iosevka-term
-                        nerd-fonts.iosevka-term-slab
-                        nerd-fonts.overpass
-                        nerd-fonts.sauce-code-pro
-                        nerd-fonts.tinos
-        ];
+    # your cat said oh no no no girl don't even think about it
+    enableACME = false;
+  };
 
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	boot.kernelPackages = pkgs.linuxPackages_latest;
+  networking.firewall.allowedTCPPorts = [80 443];
 
-	networking.hostName = "snp-des2nix";
-	networking.networkmanager.enable = true;
-        services.tailscale.enable = true;
+  stylix.enable = true;
+  stylix.autoEnable = false;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/rose-pine.yaml";
+  stylix.image = ./../wallpapers/highres/wp3.png;
+  # The generated color scheme can be viewed at /etc/stylix/palette.html on NixOS, or at ~/.config/stylix/palette.html on Home Manager.
+  #stylix.polarity = "dark";
+  stylix.fonts = {
+    #serif = {
+    #  package = pkgs.dejavu_fonts;
+    #  name = "DejaVu Serif";
+    #};
+    #sansSerif = {
+    #  package = pkgs.dejavu_fonts;
+    #  name = "DejaVu Sans";
+    #};
+    monospace = {
+      package = pkgs.nerd-fonts._0xproto;
+      name = "0xProto Nerd Font";
+    };
+    #emoji = {
+    #  package = pkgs.noto-fonts-color-emoji;
+    #  name = "Noto Color Emoji";
+    #};
+  };
+  fonts.packages = with pkgs; [
+    nerd-fonts._0xproto
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.adwaita-mono
+    nerd-fonts.agave
+    nerd-fonts.arimo
+    nerd-fonts.aurulent-sans-mono
+    nerd-fonts.bigblue-terminal
+    nerd-fonts.caskaydia-mono
+    nerd-fonts.commit-mono
+    nerd-fonts.departure-mono
+    nerd-fonts.dejavu-sans-mono
+    nerd-fonts.go-mono
+    nerd-fonts.inconsolata
+    nerd-fonts.iosevka-term
+    nerd-fonts.iosevka-term-slab
+    nerd-fonts.overpass
+    nerd-fonts.sauce-code-pro
+    nerd-fonts.tinos
+  ];
 
-	time.timeZone = "Europe/Oslo";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
-	i18n.defaultLocale = "en_US.UTF-8";
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "nb_NO.UTF-8";
-		LC_IDENTIFICATION = "nb_NO.UTF-8";
-		LC_MEASUREMENT = "nb_NO.UTF-8";
-		LC_MONETARY = "nb_NO.UTF-8";
-		LC_NAME = "nb_NO.UTF-8";
-		LC_NUMERIC = "nb_NO.UTF-8";
-		LC_PAPER = "nb_NO.UTF-8";
-		LC_TELEPHONE = "nb_NO.UTF-8";
-		LC_TIME = "nb_NO.UTF-8";
-	};
+  networking.hostName = "snp-des2nix";
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
 
-	nixpkgs.config.allowUnfree = true;
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  time.timeZone = "Europe/Oslo";
 
-	environment.systemPackages = with pkgs; [
-	];
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nb_NO.UTF-8";
+    LC_IDENTIFICATION = "nb_NO.UTF-8";
+    LC_MEASUREMENT = "nb_NO.UTF-8";
+    LC_MONETARY = "nb_NO.UTF-8";
+    LC_NAME = "nb_NO.UTF-8";
+    LC_NUMERIC = "nb_NO.UTF-8";
+    LC_PAPER = "nb_NO.UTF-8";
+    LC_TELEPHONE = "nb_NO.UTF-8";
+    LC_TIME = "nb_NO.UTF-8";
+  };
 
-	services.openssh = {
-		enable = true;
-		settings = {
-			PermitRootLogin = "no";
-		};
-		extraConfig = ''
-			Match user git
-			AllowTcpForwarding no
-			AllowAgentForwarding no
-			PasswordAuthentication no
-			PermitTTY no
-			X11Forwarding no
-		'';
-	};
-	programs.ssh = {
-		extraConfig = ''
-			Host snp-nuc1nix
-				Hostname 192.168.30.65
-				Port 22
-				User mend
-			Host localgit
-				Hostname localhost
-				User git
-				IdentityFile ~/.ssh/mend_git_local
-				IdentitiesOnly yes
-		'';
-	};
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-	# git on the server ! - https://nixos.wiki/wiki/Git
-	users.users.git = {
-		isSystemUser = true;
-		group = "git";
-		home = "/var/lib/git-server";
-		createHome = true;
-		shell = "${pkgs.git}/bin/git-shell";
-		openssh.authorizedKeys.keys = [
-			"sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILywcKsOrkjA6Zz0Nzv4zSkVSc67Yp8e1FZZql7AETTLAAAABHNzaDo= snuppy.code@pm.me"
-			"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDkgmUlpM3cGE0MDHU0QyCtspkpImLjQVpkU7ihv5P9 mend@snp-des2nix"
-		];
-	};
-	users.groups.git = {};
+  environment.systemPackages = with pkgs; [
+  ];
 
-	programs.git = {
-		enable = true;
-		config = {
-			init.defaultBranch = "main";
-                        user.name = "snuppy";
-                        user.email = "snuppy.code@pm.me";
-		};
-	};
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+    };
+    extraConfig = ''
+      Match user git
+      AllowTcpForwarding no
+      AllowAgentForwarding no
+      PasswordAuthentication no
+      PermitTTY no
+      X11Forwarding no
+    '';
+  };
+  programs.ssh = {
+    extraConfig = ''
+      Host snp-nuc1nix
+      	Hostname 192.168.30.65
+      	Port 22
+      	User mend
+      Host localgit
+      	Hostname localhost
+      	User git
+      	IdentityFile ~/.ssh/mend_git_local
+      	IdentitiesOnly yes
+    '';
+  };
 
-	# Open ports in the firewall.
-	# networking.firewall.allowedTCPPorts = [ ... ];
-	# networking.firewall.allowedUDPPorts = [ ... ];
-	# Or disable the firewall altogether.
-	# networking.firewall.enable = false;
+  # git on the server ! - https://nixos.wiki/wiki/Git
+  users.users.git = {
+    isSystemUser = true;
+    group = "git";
+    home = "/var/lib/git-server";
+    createHome = true;
+    shell = "${pkgs.git}/bin/git-shell";
+    openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAILywcKsOrkjA6Zz0Nzv4zSkVSc67Yp8e1FZZql7AETTLAAAABHNzaDo= snuppy.code@pm.me"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDkgmUlpM3cGE0MDHU0QyCtspkpImLjQVpkU7ihv5P9 mend@snp-des2nix"
+    ];
+  };
+  users.groups.git = {};
 
-	# This value determines the NixOS release from which the default
-	# settings for stateful data, like file locations and database versions
-	# on your system were taken. It‘s perfectly fine and recommended to leave
-	# this value at the release version of the first install of this system.
-	# Before changing this value read the documentation for this option
-	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-	system.stateVersion = "25.11"; # Did you read the comment?
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+      user.name = "snuppy";
+      user.email = "snuppy.code@pm.me";
+    };
+  };
 
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
