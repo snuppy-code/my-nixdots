a7bb9368f296dd2be71b409b7530e8466f72c2aa
snuppy <snuppy.code+git@pm.me>
2026-05-21 01:18:39 +0200

    fuck

--- snp-des2nix/configuration.nix
+++ snp-des2nix/configuration.nix
@@ -72,6 +72,16 @@
     locations."/" = {
       proxyPass = "http://127.0.0.1:8096";
       proxyWebsockets = true;
+      extraConfig = ''
+        proxy_buffering off;
+        proxy_request_buffering off;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Host $host;
+        proxy_read_timeout 600s;
+        proxy_send_timeout 600s;
+      '';
     };
   };
 
