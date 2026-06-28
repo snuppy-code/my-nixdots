c03a36437d4a42eafefdcb7d958fcdfd4008c093
snuppy <snuppy.code+git@pm.me>
2026-06-28 17:22:03 +0200

    ssh agent,,

--- personal-common.nix
+++ personal-common.nix
@@ -9,6 +9,40 @@
     "snuppy"
   ];
 
+  programs.ssh.startAgent = true;
+  programs.ssh.knownHosts = {
+    snp-des1nix = {
+      extraHostNames = ["snp-des1nix.tailf46592.ts.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIyu8SdTNrGB2ms9HwDvMyNR7I/8qBy1eqBqQuL2HBwP";
+    };
+    snp-lap1nix = {
+      extraHostNames = ["snp-lap1nix.tailf46592.ts.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7DrFHxJnUCJ99xb/QW4OkTEN01YgZ+HNsfCdcuvYeV";
+    };
+    snp-des2nix = {
+      extraHostNames = ["snp-des2nix.tailf46592.ts.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFIUXH2wm/Ft+ExQ2I8BuNBSc8H5nfHsw0DP6x4v/eJt";
+    };
+
+    snp-des3nix = {
+      extraHostNames = ["snp-des3nix.tailf46592.ts.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7wvmZAYtmK32+I95epLIQQL2fe/GwR4KFyRf3mwYHy";
+    };
+    snp-nuc1nix = {
+      extraHostNames = ["snp-nuc1nix.tailf46592.ts.net"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILOzBjkxKePr1Gyr4KeQ/4J1CMQfLT3CSHygf0rCGt95";
+    };
+
+    github = {
+      hostNames = ["github.com"];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+    };
+    github-ecdsa = {
+      hostNames = ["github.com"];
+      publicKey = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=";
+    };
+  };
+
   services.open-webui.enable = true;
 
   sops.secrets.snuppy-password.neededForUsers = true;
